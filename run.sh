c129f3b7acfabf981edb8e59cf02583885306d27
Nguyen Minh Ly <20521592@gm.uit.edu.vn>
2022-08-22 17:58:03 +0700

    add ctsv

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 # . venv/bin/activate
-echo 'get common news'
+# echo 'get common news'
 python3 src/get_news_common.py
 
-echo 'get cs news'
+# echo 'get cs news'
 python3 src/get_news_cs.py
 
-echo 'get special news'
+# echo 'get special news'
 python3 src/get_news_special.py
+
+python3 src/get_news_ctsv.py
--- src/get_news_ctsv.py
+++ src/get_news_ctsv.py
@@ -6,22 +6,21 @@ import json
 from send_telegram import *
 import asyncio
 
-url =  'https://student.uit.edu.vn/'
+url =  'https://ctvs.uit.edu.vn/'
 page = urllib.request.urlopen(url)
 soup = BeautifulSoup(page, 'html.parser')
-domain = 'https://student.uit.edu.vn/'
+domain = ''
 
 class_name = [
     'views-row views-row-1 views-row-odd views-row-first', 
     'views-row views-row-2 views-row-even', 
     'views-row views-row-3 views-row-odd', 
+    'views-row views-row-4 views-row-even views-row-last', 
     'views-row views-row-4 views-row-even', 
     'views-row views-row-5 views-row-odd', 
     'views-row views-row-6 views-row-even', 
     'views-row views-row-7 views-row-odd', 
-    'views-row views-row-8 views-row-even', 
-    'views-row views-row-9 views-row-odd', 
-    'views-row views-row-10 views-row-even views-row-last'
+    'views-row views-row-8 views-row-even views-row-last'
 ]
 
 
@@ -35,12 +34,26 @@ class uit_feed:
 
 
 def get_new(class_name):
-    new_feed = soup.find('li', class_=class_name).find('a')
-    link = domain + new_feed.get('href')
-    title = new_feed.getText()
-    feed =uit_feed(title=title, link=link)
-    return feed
+    new_feed = soup.find_all('li', class_=class_name)
+    list_feed = []
+    for i in new_feed:
+        i = i.find('a')
+        link = domain + str(i.get('href'))
+        title = i.getText()
+        feed =uit_feed(title=title, link=link)
+        list_feed.append(feed)
+    return list_feed
 
+def get_new_hot():
+    new_feed = soup.find_all('div', class_='top-title')
+    list_feed = []
+    for i in new_feed:
+        i = i.find('a')
+        link = domain + str(i.get('href'))
+        title = i.getText()
+        feed =uit_feed(title=title, link=link)
+        list_feed.append(feed)
+    return list_feed
 
 def check_in(feed, list_old_feed):
     for i in list_old_feed: 
@@ -50,12 +63,13 @@ def check_in(feed, list_old_feed):
 
 
 # get new feed
-list_new_feed = []
+list_new_feed = get_new_hot()
 for i in tqdm(class_name):
-    list_new_feed.append(get_new(class_name=i))
+    list_new_feed += get_new(class_name=i)
+
 
 # read old feed
-f = open('data/current_feed_common.json')
+f = open('data/current_feed_ctsv.json')
 data = json.load(f)
 f.close()
 list_old_feed = [uit_feed(i.get('title'), i.get('link')) for i in data['info']]
@@ -76,5 +90,6 @@ for i in new_feed:
 
 # save current feed
 list_new_feed = [i.__dict__ for i in list_new_feed]
-with open("data/current_feed_common.json", "w", encoding='utf-8') as outfile:
+with open("data/current_feed_ctsv.json", "w", encoding='utf-8') as outfile:
     json.dump({"info":list_new_feed}, outfile, ensure_ascii=False, indent=4)
+
